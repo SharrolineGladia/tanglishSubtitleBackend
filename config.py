f5b8d0f0bd7dbaa10edc5bd773d1175f0eabd826
SharrolineGladia <sharrolglee@gmail.com>
2025-06-03 21:56:36 +0530

    Add Hugging Face Docker deployment files

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
     DEBUG = os.environ.get('DEBUG') or False
     HOST = os.environ.get('HOST') or '0.0.0.0'
-    PORT = int(os.environ.get('PORT') or 5000)
+    PORT = int(7860)
     
     # File upload settings
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/temp')
